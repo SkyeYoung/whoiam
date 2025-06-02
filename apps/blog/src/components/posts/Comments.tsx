fe372aacc90c346ccb3ee9be9514fd5f345cb504
Skye Young <iskyex@outlook.com>
2025-06-02 15:30:54 +0800

    feat(Comments): add loading status

---
+++ apps/blog/src/components/posts/Comments.tsx
@@ -0,0 +1,52 @@
+import Giscus from '@giscus/react';
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+
+export const Comments = () => {
+  const [loading, setLoading] = useState(true);
+  const giscusRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const giscus = giscusRef.current;
+    if (!giscus) return;
+    const observer = new MutationObserver(() => {
+      const content = giscus.children[0]?.shadowRoot;
+      if (!!content) {
+        setTimeout(() => setLoading(false), 1500);
+        observer.disconnect();
+      }
+    });
+    observer.observe(giscus, {
+      childList: true,
+      subtree: true,
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <section className="relative min-h-80">
+      <div
+        className={clsx(
+          'absolute top-0 left-0 z-10 rounded-xl w-full h-full bg-zinc-800/5 flex items-center justify-center text-zinc-600 transition-all duration-300 ease-in-out',
+          loading ? 'opacity-100' : 'opacity-0 h-0'
+        )}
+      >
+        Loading comments...
+      </div>
+      <div id="comments" ref={giscusRef}>
+        <Giscus
+          repo="SkyeYoung/SkyeYoung"
+          repoId="R_kgDOG-k4ew"
+          category="blog"
+          categoryId="DIC_kwDOG-k4e84Cq5ge"
+          mapping="url"
+          reactionsEnabled="1"
+          emitMetadata="1"
+          theme="light"
+          lang="en"
+        />
+      </div>
+    </section>
+  );
+};
