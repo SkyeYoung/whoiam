362fc29b4add08aa34e632b9b985ce51f1f01631
Skye Young <iskyex@outlook.com>
2025-06-07 21:24:10 +0800

    feat: figure img

--- apps/blog/src/components/md/Image.tsx
+++ apps/blog/src/components/md/Image.tsx
@@ -3,20 +3,31 @@ import { inferRemoteSize } from 'astro:assets';
 import { omit } from 'lodash-es';
 
 type BasicImageProps = React.ImgHTMLAttributes<HTMLImageElement>;
+
+const BasicImage = (props: BasicImageProps) => {
+  return (
+    <figure>
+      <img
+        className="cursor-zoom-in rounded border-solid border-gray-300 border-1 object-cover"
+        {...omit(props, 'node')}
+      />
+      <figcaption className="text-sm text-gray-500 mt-2">
+        <span className="select-none">— </span>
+        {props.alt}
+      </figcaption>
+    </figure>
+  );
+};
+
 const LocalImage = async (props: BasicImageProps) => {
   const img = await getLocalImage(props.src!);
   const placeholder = await getLocalPlaceholder(props.src!);
-
-  return (
-    <img {...omit(props, 'node')} {...img.attrs} style={placeholder.style} />
-  );
+  return <BasicImage {...props} {...img.attrs} style={placeholder.style} />;
 };
 
 const RemoteImage = async (props: BasicImageProps) => {
   const size = await inferRemoteSize(props.src!);
-  return (
-    <img {...omit(props, 'node')} {...size} loading="lazy" decoding="async" />
-  );
+  return <BasicImage {...props} {...size} loading="lazy" decoding="async" />;
 };
 
 const Image = (props: BasicImageProps) => {
